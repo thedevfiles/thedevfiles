3d0f054c692f4c7a7df4c33485935f2e3514a4dc
Jonathan Bernardi <jon@jonbernardi.com>
2018-11-14 10:43:17 -0800

    fix icon generation

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Helmet from 'react-helmet'
 import {graphql} from "gatsby"
-git import {DiscussionEmbed} from "disqus-react"
+import {DiscussionEmbed} from "disqus-react"
 import Layout from "../components/layout"
 import PostTags from "../components/post/tags";
 import config from "../../data/config";
