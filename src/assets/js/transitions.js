77289196657ae9a04dbca9efb7029425725a74b9
Jonathan Bernardi <jon@jonbernardi.com>
2025-06-25 12:04:12 -0700

    manual tracking not needed

--- src/assets/js/transitions.js
+++ src/assets/js/transitions.js
@@ -3,10 +3,4 @@ document.addEventListener('astro:page-load', function () {
     if (document.getElementById('disqus_thread')) {
         lazyLoadDisqus();
     }
-    if (zaraz) {
-        zaraz.track('page_view', {
-            page_location: window.location,
-            page_title: document.title,
-        });
-    }
 })
