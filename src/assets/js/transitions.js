d461d873ceb619b1655fa5dbbd6604aea4537ff9
Jonathan Bernardi <jon@jonbernardi.com>
2025-06-25 10:00:22 -0700

    add page view track

--- src/assets/js/transitions.js
+++ src/assets/js/transitions.js
@@ -3,4 +3,10 @@ document.addEventListener('astro:page-load', function () {
     if (document.getElementById('disqus_thread')) {
         lazyLoadDisqus();
     }
+    if (zaraz) {
+        zaraz.track('page_view', {
+            page_location: window.location,
+            page_title: document.title,
+        });
+    }
 })
