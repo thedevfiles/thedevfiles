ff3c8fabd75a42b3a0071052a2bac60f958b5561
Jonathan Bernardi <jon@jonbernardi.com>
2025-06-25 09:40:48 -0700

    remove module

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import serviceWorker from "astrojs-service-worker";
 
-import partytown from '@astrojs/partytown';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
